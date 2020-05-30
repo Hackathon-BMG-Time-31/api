dd94351a5a3935ec5d03861b9bd8214287d27509
Vinicius Sampaio <viniciuscastrosampaio@gmail.com>
2020-05-30 20:52:02 -0300

    habilitando cors

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var expressValidator = require("express-validator");
+var flash = require("connect-flash");
+var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-const settings = require('./credentials.json');
+const settings = require("./credentials.json");
+var cors = require("cors");
 
-const mongoConnectionString = 'mongodb+srv://time_31:MYhgUjV0MiOTClor@cluster0-y0e9d.mongodb.net/test?retryWrites=true&w=majority';
+const mongoConnectionString =
+  "mongodb+srv://time_31:MYhgUjV0MiOTClor@cluster0-y0e9d.mongodb.net/test?retryWrites=true&w=majority";
 
-console.log('mongoConnectionString => ', mongoConnectionString)
+console.log("mongoConnectionString => ", mongoConnectionString);
 
 mongoose.connect(mongoConnectionString);
 var db = mongoose.connection;
-var users = require('./routes/users');
-var nodemailer = require('nodemailer');
+var users = require("./routes/users");
+var nodemailer = require("nodemailer");
 
 // Init App
 var app = express();
 
 // View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-app.set('view engine', 'handlebars');
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({ defaultLayout: "layout" }));
+app.set("view engine", "handlebars");
 
 // BodyParser Middleware
 app.use(bodyParser.json());
@@ -36,29 +38,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Express Session
-app.use(session({
-    secret: 'session secret key',
+app.use(
+  session({
+    secret: "session secret key",
     saveUninitialized: true,
-    resave: true
-}));
+    resave: true,
+  })
+);
 
 // Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // Connect Flash
 app.use(flash());
 
+app.use(cors({ origin: 'http://localhost:3000' , credentials :  true}));
+app.options('*', cors());
 
-app.use('/users', users);
+
+app.use("/users", users);
 
 // Set Port
-app.set('port', (process.env.PORT || 5000));
+app.set("port", process.env.PORT || 5000);
 
-app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+app.listen(app.get("port"), function () {
+  console.log("Server started on port " + app.get("port"));
 });
